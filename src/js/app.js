1adadfeb4db01be66b149a0affe068ea3ad1576b
yurayarosh <yura12323@gmail.com>
2019-09-23 14:32:04 +0300

    edits

--- src/js/app.js
+++ src/js/app.js
@@ -16,6 +16,7 @@ import setTextareaHeight from './components/setTextareaHeight';
 import setPopups from './components/setPopups';
 import setScrollbar from './components/setScrollbar';
 import toggleFormAdressBlock from './components/toggleFormAdressBlock';
+// import toggleHeader from './components/toggleHeader';
 
 $(() => {
   sayHello();
@@ -32,4 +33,7 @@ $(() => {
   setPopups();
   setScrollbar();
   toggleFormAdressBlock();
+  // toggleHeader();
 });
+
+window.setLazy = setLazy;
